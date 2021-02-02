21852e70ac6be3160182ddd92a01bb5c00bba407
Sam Mendis <samuel.mendis@lincoln.ox.ac.uk>
2021-02-02 15:20:01 +0000

    editing controller further

--- attempt2/gui.py
+++ attempt2/gui.py
@@ -27,7 +27,8 @@ root = Tk()
 '''Sorting out date and time'''
 date_time = datetime.now()
 Date = '%s/%s/%s' % (date_time.day, date_time.month, date_time.year)
-Time = '%s:%s:%s' % (date_time.hour, date_time.minute, date_time.second)
+Current_Time = '%s:%s:%s' % (
+    date_time.hour, date_time.minute, date_time.second)
 
 # Something needed for Temperature Sensor
 '''
@@ -90,6 +91,7 @@ def temp_control(Temp):
 
     if Temp.actual < Temp.desired:
         "switch on Heater by closing SSR"
+        print('heater on')
         # Switching GPIO 6 on
     else:
         '''switch off heater'''
@@ -101,9 +103,29 @@ def temp_control(Temp):
 
 def control(Temp, Time, Gas, Voltage):
     t0 = time.time()
+    Tend = time.time()+Time.desired
+    Temperature = [[Date, Current_Time], [
+        'Time in seconds/s', 'Temperature/deg C'], [0, 20]]
+    Cells_V = [[Date, Current_Time], [
+        'Time in seconds/s', 'Cell 1 Voltage/V', 'Cell 2 Voltage/V', 'Cell 3 Voltage/V', 'Cell 4 Voltage/V', 'Cell 5 Voltage/V', 'Cell 6 Voltage/V'], [0, 0, 0, 0, 0, 0, 0]]
     time.sleep(1)
-    Time.actualv(round((time.time()-t0), 2))
-    Temperature = [Time.actual, temp_control(Temp)]
+
+    # This while loop is where all the control neeeds to be done, includig gas and voltage control
+    while time.time() < Tend:
+        Time.actualv(round((time.time()-t0), 2))
+        Temperature.append([Time.actual, temp_control(Temp)])
+
+        if (time.time()-t0) < ((Tend-t0)/5):
+            time.sleep(0.1)
+        else:
+            time.sleep(1)
+
+    file = open('Temperature_data.csv', 'a+', newline='')
+
+    with file:
+        write = csv.writer(file)
+        write.writerows(Temperature)
+
     print(Temperature)
     end = Time.desired + t0
 
--- attempt2/testfile.py
+++
@@ -1,8 +0,0 @@
-import time
-from datetime import datetime
-
-date_time = datetime.now()
-Date = '%s/%s/%s' % (date_time.day, date_time.month, date_time.year)
-Time = '%s:%s:%s' % (date_time.hour, date_time.minute, date_time.second)
-Temperature = [Time + " " + Date, 1]
-print(Temperature)
