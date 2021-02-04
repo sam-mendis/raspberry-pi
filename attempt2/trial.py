449bc22fe392372eba7de3c085004929426e61eb
Sam Mendis <samuel.mendis@lincoln.ox.ac.uk>
2021-02-04 09:36:26 +0000

    Create trial.py

---
+++ attempt2/trial.py
@@ -0,0 +1,26 @@
+from gui import T
+from multiprocessing import Process
+import time
+
+T = 5
+
+
+def func1():
+    print('func1: starting')
+    time.sleep(3)
+    print('func1: finishing')
+
+
+def func2():
+    print('func2: starting')
+    time.sleep(3)
+    print('func2: finishing')
+
+
+if __name__ == '__main__':
+    p1 = Process(target=func1)
+    p1.start()
+    p2 = Process(target=func2)
+    p2.start()
+    p1.join()
+    p2.join()
