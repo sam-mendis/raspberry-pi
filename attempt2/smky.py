9a2cd78b68f44e55b4dc7b962190f74468251c41
Sam Mendis <samuel.mendis@lincoln.ox.ac.uk>
2021-03-29 08:54:51 +0100

    Update smky.py

--- attempt2/smky.py
+++ attempt2/smky.py
@@ -9,3 +9,30 @@ import pandas as pd
 
 #adapter = PrologixAdapter('/dev/ttyUSB0')
 #sourcemeter = Keithley2400(adapter.gpib(4))
+keithley = Keithley2400("GPIB::1")
+
+
+def k_measure_v():
+    keithley.measure_voltage()
+
+    V = keithley.voltage
+
+    return V
+
+
+def k_measure_I():
+    keithley.measure_current()
+
+    I = keithley.current
+
+    return I
+
+
+def k_set_V(v):
+    keithley.apply_voltage()
+    keithley.ramp_to_voltage(v, steps=1, pause=0.01)
+
+
+def k_set_I(i):
+    keithley.apply_current()
+    keithley.ramp_to_current(i, steps=1, pause=0.01)
